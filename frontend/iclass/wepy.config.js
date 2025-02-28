99227866d9f37e90e829258b5878959b27c55135
sule <aeonsule@outlook.com>
2025-02-28 22:50:25 +0800

    first

---
+++ frontend/iclass/wepy.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+var prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: !prod,
+  static: ['static'],
+  build: {
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src'),
+      '@/components': path.join(__dirname, 'src/components'),
+      '@/pages': path.join(__dirname, 'src/pages'),
+      counter: path.join(__dirname, 'src/components/counter')
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    less: {
+      compress: prod
+    },
+    babel: {
+      sourceMap: true,
+      presets: [
+        '@babel/preset-env'
+      ],
+      plugins: [
+        '@wepy/babel-plugin-import-regenerator'
+      ]
+    }
+  },
+  plugins: [],
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
